91f7bbeb8e2c922b9fe0d06eb64bfb2e593e7383
madi <maditabon33@gmail.com>
2024-01-08 12:02:49 -0800

    Adding in class work

---
+++ components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return(
+    <footer className={styles.footer}>
+            <h2>Created by Rimando</h2>
+    </footer>
+  )
+}
---
+++ pages/About.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+
+export default function About(){
+  return(
+        <>
+            <Header/>
+            Test 
+            <Footer/>
+        </>
+
+  )
+}
